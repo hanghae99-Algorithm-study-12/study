98f89572814f87bc71c1c78a01b7f6289a37f8d3
jeonho <jhkang1995@naver.com>
2022-01-17 10:56:30 +0900

    .

---
+++ 강전호/1주차/과제/2회차/reverse-linked-list.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+       self.val = val
+       self.next = next
+
+
+
+
+a=ListNode(23)
+print(a)
--- 강전호/1주차/수업/pythonProject/chap06_1.py
+++ 강전호/1주차/수업/pythonProject/chap06_1.py
@@ -31,6 +31,23 @@ def isPalindrome3(str):
     isPalin=True
 
     return isPalin
-if __name__ == "__main__":
+
+# 두수의 합
+# 덧셈을 하여 타겟을 만들 수 있는 배열의 두 숫자 인덱스를 리턴하라
+# input = [2, 7, 11, 15]
+# target 9
+# output = [0, 1]
+def twoSome(nums,target): ## 브루트포스 (완전탐색)
+    answer=[]
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if(nums[i]+nums[j]==target):
+                return [i,j]
+
+def twoSome2(nums,target):
+    for i,n in enumerate(nums):
+        complement = target-n
+        if complement in nums[i+1:]: # 기준 + n = target  기준이 원하는 수 = complement  -> complement 가 현재 배열에 기준 뒤에 존재하는가?
+            return [nums.index(n),nums[i+1:].index(complement)+(i+1)]  # nums[i+1:] -> 배열의 시작인덱스가 i+1부터 시작이라 뒤에 i+1을 더해줌줌if __name__ == "__main__":
     # mom, rotator, ... 앞이나 뒤에서 읽어도 똑같아.
-    print(isPalindrome("bb"))
+print(twoSome([6,6],12))
